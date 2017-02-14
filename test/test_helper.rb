726de1a133574353d60bda2bbc077fdded2a5b6e
Chaz Larson <chaz@chazlarson.com>
2017-02-14 23:52:23 +0000

    Added user signup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -47,3 +47,27 @@ def teardown_universal
   Capybara.reset_sessions!
   Capybara.use_default_driver
 end
+
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/ParameterLists
+def sign_up_user(name_u, name_l, name_f, e, p1, p2)
+  visit root_path
+  assert page.has_link?('Sign up now!', href: new_user_registration_path)
+  click_on 'Sign up now!'
+  assert page.has_css?('title', text: full_title('New User'), visible: false)
+  assert page.has_css?('h1', text: 'New User')
+  assert page.has_text?('password management program')
+  assert page.has_text?('create much better passwords')
+  assert page.has_link?('KeePassX', href: 'http://www.keepassx.org')
+  fill_in('Last name', with: name_l)
+  fill_in('First name', with: name_f)
+  fill_in('Username', with: name_u)
+  fill_in('Email', with: e)
+  fill_in('Password', with: p1) # not yet changed
+  fill_in('Password confirmation', with: p2)
+  click_button('Sign up')
+end
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/ParameterLists
